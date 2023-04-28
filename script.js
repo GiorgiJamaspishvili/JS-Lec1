66dcfda7b90030e2baabe6f6adc0215e7f686841
GiorgiJamaspishvili <126399819+GiorgiJamaspishvili@users.noreply.github.com>
2023-04-28 18:54:32 +0400

    complete

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-let number = 100;
+// HomeWork 1
 
-if ( number < 50){
-    console.log('Less than 50');
-}
-else if ( number > 20){
-    console.log('More than 20');
-}
-else {
-    console.log('Error');
-}
+// let number = 100;
+
+// if ( number < 50){
+//     console.log('Less than 50');
+// }
+// else if ( number > 20){
+//     console.log('More than 20');
+// }
+// else {
+//     console.log('Error');
+// }
+
+
+
+
+
+// // HomeWork 2
+
+// let userName = "Mariam";
+
+// 1.
+// let result = ('userName =', !"Mariam") ? 'True' : 'Error';
+// console.log(result);
+
+
+// 2.
+// if (userName == "Mariam") {
+//     console.log("True");
+// }
+// else {
+//     console.log("Error");
+// }
+
+
+
+
+
+// // HomeWork 3
+
+// switch (userName) {
+//     case "Mariam":
+//         console.log('True');
+//         break;
+//     default:
+//         console.log('Error');
+// }
+
 
 
